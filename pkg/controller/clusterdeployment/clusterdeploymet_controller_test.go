02aaea9cc0e624cead489e30db69a52bcfadaf64
boranx <boran.seref@gmail.com>
2020-02-20 15:39:17 +0100

    remove unnecessary test

--- pkg/controller/clusterdeployment/clusterdeploymet_controller_test.go
+++ pkg/controller/clusterdeployment/clusterdeploymet_controller_test.go
@@ -82,16 +82,6 @@ func TestReconcile(t *testing.T) {
 			setupGCPMock: func(r *mockGCP.MockClientMockRecorder) { gomock.Any() },
 		},
 		{
-			name:        "failed to get orgParentFolderID from configmap, moving with default",
-			expectedErr: nil,
-			localObjects: []runtime.Object{
-				builders.NewTestClusterDeploymentBuilder().GetClusterDeployment(),
-				builders.NewTestSecretBuilder(orgGcpSecretName, operatorNamespace, "testCreds").GetTestSecret(),
-				builders.NewTestSecretBuilder(gcpSecretName, testNamespace, "testCreds").GetTestSecret(),
-			},
-			setupGCPMock: func(r *mockGCP.MockClientMockRecorder) { gomock.Any() },
-		},
-		{
 			name:        "no billing key in secret",
 			expectedErr: fmt.Errorf("GCP credentials secret gcp-project-operator did not contain key billingaccount"),
 			localObjects: []runtime.Object{
