5916223eda99b5b6f0dece6686cfe778cc35fcb6
Manuel Dewald <mdewald@redhat.com>
2020-03-09 16:36:11 +0100

    Add tests for project id generation (#17)
    * move id generation to its own function for easier testing
    * add test file for adapter tests
    
    Co-authored-by: Manuel Dewald <m.dewald@redhat.com>

---
+++ pkg/controller/projectreference/projectreference_adapter_test.go
@@ -0,0 +1,38 @@
+package projectreference_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/openshift/gcp-project-operator/pkg/controller/projectreference"
+)
+
+var _ = Describe("ProjectreferenceAdapter", func() {
+	Context("generated project names", func() {
+		It("are shorter than 30 characters", func() {
+			projectID, err := GenerateProjectID()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(projectID)).To(BeNumerically("<=", 30))
+		})
+
+		It("are longer than 6 characters", func() {
+			projectID, err := GenerateProjectID()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(projectID)).To(BeNumerically(">=", 6))
+		})
+
+		It("start with a lowercase letter", func() {
+			projectID, err := GenerateProjectID()
+			Expect(err).NotTo(HaveOccurred())
+			Expect("abcdefghijklmnopqrstuvwxyz").To(ContainSubstring(string(projectID[0])))
+		})
+
+		It("contains only lowercase letters, numbers or hyphens", func() {
+			projectID, err := GenerateProjectID()
+			Expect(err).NotTo(HaveOccurred())
+			for _, char := range projectID {
+				Expect("abcdefghijklmnopqrstuvwxyz1234567890-").To(ContainSubstring(string(char)))
+			}
+		})
+	})
+})
