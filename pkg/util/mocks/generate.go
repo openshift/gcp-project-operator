3369ef429584419d5c8f0121adf7be5e74bf3071
Brandon Palm <bpalm@redhat.com>
2025-11-13 15:20:08 -0600

    mockgen deprecated: use uber-go/mock instead

--- pkg/util/mocks/generate.go
+++ pkg/util/mocks/generate.go
@@ -4,8 +4,8 @@ package mocks
 // mock fixtures in this package that come from source files outside of this project (otherwise the
 // directives should go in the source files themselves).
 // Run `make generate` from the project root.
-// Dependency: mockgen, qua:
-//    GO111MODULE=on go get github.com/golang/mock/mockgen@latest
+// Dependency: mockgen, via:
+//    go install go.uber.org/mock/mockgen@v0.6.0
 
 //go:generate mockgen -destination ./cr-client.go -package mocks sigs.k8s.io/controller-runtime/pkg/client Client
 //go:generate mockgen -destination ./status-writer.go -package mocks sigs.k8s.io/controller-runtime/pkg/client StatusWriter
